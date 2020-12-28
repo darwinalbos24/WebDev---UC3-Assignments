1893cc3b555c0458599fa0da31f1675dd3674ea4
albosdarwin24 <darwinalbos2124@gmail.com>
2020-12-28 23:02:41 +0800

    Added LO4 files, modified LO3

--- LO3.js
+++ LO3.js
@@ -14,20 +14,44 @@ Array.from(btns).forEach(function(btn){
 // Event Bubbling - delete books
 
 const list = document.querySelector("#book-list ul");
+const forms = document.forms;
 
-list.addEventListener("click", function(e){
-    if (e.target.className == "delete");
+list.addEventListener('click', function(e){
+    if (e.target.className == 'delete'){
     const li = e.target.parentElement;
     list.removeChild(li);
+    }
 });
 
 // Interacting with forms - add books
 
-const addForm = document.forms["add-book"];
+const addForm = forms['add-book'];
 
-addForm.addEventListener("submit", function(e){
+addForm.addEventListener('submit', function(e){
     e.preventDefault();
+    
+    // create elements
     const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    // add text content
+    bookName.textContent = value;
+    deleteBtn.textContent = 'delete';
+    
+
+    // add classes
+
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    // append to document
+
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
 
 });
+
---
+++ LO4.js
@@ -0,0 +1,21 @@
+// Event Bubbling - delete books
+
+const list = document.querySelector("#book-list ul");
+
+list.addEventListener("click", function(e){
+    if (e.target.className == "delete");
+    const li = e.target.parentElement;
+    list.removeChild(li);
+});
+
+// Interacting with forms - add books
+
+const addForm = document.forms["add-book"];
+
+addForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
+
+});
+
